f78f58c3a03b15a1a701e7277fb783b87c5f84e6
Ursoo <ursu.calin18@gmail.com>
2025-02-14 00:42:20 +0200

    added char counting method

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ def main():
     path = "./books/frankenstein.txt"
     book_content = read_book_contents(book_path=path)
     word_count = count_words(string=book_content)
-    print(f"{word_count} words")
+    char_dict = count_character_occurence(string=book_content)
+    print(f"words_counted: {word_count},\n")
+    print(f"chars_counted: {char_dict}, \n")
 
 def read_book_contents(book_path):
     with open(book_path) as f:
@@ -14,5 +16,14 @@ def read_book_contents(book_path):
 def count_words(string: str):
     return len(string.split())
 
+def count_character_occurence(string: str):
+    char_occurence = {}
+    for character in string:
+        if character.lower() in char_occurence:
+            char_occurence[character.lower()] += 1
+        else:
+            char_occurence[character.lower()] = 1
+    
+    return char_occurence
 
 main()
