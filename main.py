7910b30f0af6dad7c23f0ddc9c3f0d4b19bf7d54
Ursoo <ursu.calin18@gmail.com>
2025-02-14 00:36:02 +0200

    added word counting method

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+
+def main():
+    path = "./books/frankenstein.txt"
+    book_content = read_book_contents(book_path=path)
+    word_count = count_words(string=book_content)
+    print(f"{word_count} words")
+
+def read_book_contents(book_path):
+    with open(book_path) as f:
+        content = f.read()
+    return content
+
+
+def count_words(string: str):
+    return len(string.split())
+
+
+main()
