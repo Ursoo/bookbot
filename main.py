81537eb04cac5a9b2f9eb45665ab4a7a32b21d9b
Ursoo <ursu.calin18@gmail.com>
2025-02-14 00:50:35 +0200

    added a formatted report method

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ def main():
     path = "./books/frankenstein.txt"
     book_content = read_book_contents(book_path=path)
     word_count = count_words(string=book_content)
-    char_dict = count_character_occurence(string=book_content)
-    print(f"words_counted: {word_count},\n")
-    print(f"chars_counted: {char_dict}, \n")
+    char_count_dict = count_character_occurence(string=book_content)
+    #print(f"words_counted: {word_count}")
+    #print(f"chars_counted: {char_dict}")
+    print_report(word_count= word_count, char_count=char_count_dict)
 
 def read_book_contents(book_path):
     with open(book_path) as f:
@@ -25,5 +26,14 @@ def count_character_occurence(string: str):
             char_occurence[character.lower()] = 1
     
     return char_occurence
+
+def print_report(word_count, char_count):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
+    print("\n")
+    for key in char_count:
+        if key.isalpha():
+            print(f"The '{key}' character was found {char_count[key]} times")
+    print("--- End report ---")
 
 main()
